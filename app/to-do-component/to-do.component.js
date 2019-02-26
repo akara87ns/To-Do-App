ce9bab5cf28711ec6fb0f052bd02a6a4d9449801
Aleksandar Tadic <aleksandartadic.ns@gmail.com>
2019-02-26 20:10:39 +0100

    CSS styles added to the application

--- app/to-do-component/to-do.component.js
+++ app/to-do-component/to-do.component.js
@@ -2,6 +2,8 @@ angular.module('toDoApp')
     .component('toDoApp', {
         templateUrl: 'app/to-do-component/to-do.template.html',
         controller: function ToDoController($scope) {
+            $scope.li = false;
+
             $scope.tasks = []
 
             $scope.addToDoTask = function(task) {
@@ -9,6 +11,7 @@ angular.module('toDoApp')
                     $scope.tasks.push(task);
                 }
                 $scope.newTask = "";
+                console.log($scope.li);
             }
 
             $scope.removeToDoTask = function(task) {
